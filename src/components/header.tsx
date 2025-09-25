a61988060408a27491f560019ece4580b7aef969
gabsolano <gabrielsolano2006@gmail.com>
2025-09-24 22:06:17 -0300

    Feat: created Header component

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-8 text-center shadow-lg">
+      <h1 className="text-4xl font-bold mb-2">
+        Seja bem vindo ao StudyLog
+      </h1>
+      <h2 className="text-xl font-light text-gray-300">
+        O melhor tracker de estudos
+      </h2>
+    </header>
+  );
+}
