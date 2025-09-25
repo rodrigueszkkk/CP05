f6224603dbc217e7d44867489e72e8b8088fd578
LeandroGuaridoOliveira <leandro.guarido@gmail.com>
2025-09-24 22:13:08 -0300

    Feat: created footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white p-4 mt-auto">
+        <div className="container mx-auto text-center">
+            <p>StudyLog. Todos os direitos reservados.</p>
+        </div>
+        </footer>
+    );
+}
+
